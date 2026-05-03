826bff10f5c1c67fda48e154f91bba133022e8e1
coderwoogi <111699952+coderwoogi@users.noreply.github.com>
2026-05-03 20:12:20 +0900

    fix(DB): Correct black market tracker template

--- modules/mod-blackmarket/data/sql/db-world/updates/2026_05_03_00_blackmarket_tracker_item.sql
+++ modules/mod-blackmarket/data/sql/db-world/updates/2026_05_03_00_blackmarket_tracker_item.sql
@@ -6,14 +6,17 @@ UPDATE `item_template`
 SET
     `class` = 0,
     `subclass` = 0,
+    `Quality` = 1,
+    `Flags` = 64,
     `name` = '암상인 추적기',
     `description` = '사용 시 현재 암상인의 위치 정보를 우편으로 받습니다.',
+    `ItemLevel` = 0,
     `InventoryType` = 0,
     `spellid_1` = 46797,
     `spelltrigger_1` = 0,
     `spellcharges_1` = 0,
-    `spellcooldown_1` = -1,
+    `spellcooldown_1` = 5000,
     `spellcategory_1` = 0,
-    `spellcategorycooldown_1` = -1,
+    `spellcategorycooldown_1` = 5000,
     `ScriptName` = 'item_blackmarket_tracker'
 WHERE `entry` = 600025;
